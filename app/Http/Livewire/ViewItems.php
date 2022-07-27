e8810d620d67a2d0c562ac20ce9765bcbc20af9c
Jamees Nganga <jamesnight1994@gmail.com>
2022-07-27 13:33:47 +0300

    buying logic foundation

--- app/Http/Livewire/ViewItems.php
+++ app/Http/Livewire/ViewItems.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Livewire;
 
-use App\Models\Slot;
+use App\Models\{Item,Slot};
 use Livewire\Component;
 
 class ViewItems extends Component
 {
     public $cols,$rows;
+
+    public array $selectedItem, $denoms;
 
     public function __construct() {
         $slots = Slot::has('item')->with(['item'])->get();
@@ -16,9 +18,98 @@ class ViewItems extends Component
         $rows = collect($slots)->groupBy('row')->toArray();
         // dd();
         
+        $this->cart = [];
+        $this->denoms = [
+            [
+                'value' => 5,
+                'type' => 'cent',
+                'image' => '',
+            ],
+            [
+                'value' => 10,
+                'type' => 'cent',
+                'image' => '',
+            ],
+            [
+                'value' => 25,
+                'type' => 'cent',
+                'image' => '',
+            ],
+            [
+                'value' => 50,
+                'type' => 'cent',
+                'image' => '',
+            ],
+            [
+                'value' => 1,
+                'type' => 'dollar',
+                'image' => '',
+            ],
+            [
+                'value' => 2,
+                'type' => 'dollar',
+                'image' => '',
+            ],
+            [
+                'value' => 5,
+                'type' => 'dollar',
+                'image' => '',
+            ],
+            
+            [
+                'value' => 10,
+                'type' => 'dollar',
+                'image' => '',
+            ],
+            [
+                'value' => 20,
+                'type' => 'dollar',
+                'image' => '',
+            ],
+            [
+                'value' => 50,
+                'type' => 'dollar',
+                'image' => '',
+            ],
+            [
+                'value' => 100,
+                'type' => 'dollar',
+                'image' => '',
+            ]
+        ];
         $this->cols = $cols;
         $this->rows = $rows;
     }
+
+    public function buy(array $denoms)
+    {
+        $cash = json_decode($denoms);
+        $cash = collect($cash);
+        $total = 0;
+        
+        // take the cents see how many dollars they add up to
+        $cash->where('type','cent')->each(function($denom,$key) use($total){
+            $total += $denom['value'];
+
+        });
+        // convert to dollars
+        $total = $total/100;
+
+        // add up the dollars
+        $cash->where('type','dollar')->each(function($denom,$key) use($total){
+            $total += $deno['value'];
+
+        });
+
+        dd($total);
+        
+    }
+
+    public function selectItem(array $item)
+    {
+        $this->selectedItem[] = $item;
+    }
+    
     public function render()
     {
         return view('livewire.view-items');
--- resources/views/livewire/view-items.blade.php
+++ resources/views/livewire/view-items.blade.php
@@ -12,13 +12,37 @@
         @foreach($rows as $row_key => $row)
         <tr>
         @foreach($cols as $col_key => $col)
-        <td>{{ $row_key.$col_key }}</td>
         <td>
-            <img src="{{ $col[0]['item']['image'] }}" alt="">
+            <strong style="text-align:center">{{ $col[0]['item']['name'] }}</strong><br><br>
+            <img style="text-align:center" src="{{ $col[0]['item']['image'] }}" alt=""><br><br>
+            Slot:{{ $row_key.$col_key }}
+            Price: {{ $col[0]['item']['price'] }}
+            <button wire:click="selectItem(@json($col[0]['item']))">
+                Select Item
+            </button>
         </td>
         @endforeach
         </tr>
         @endforeach
     </tbody>
-    </table>
+    </table><br><br><br>
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+
+
 </div>
